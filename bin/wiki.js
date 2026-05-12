a38d01d0d66c9d631de3864cf7716e2b36a74966
ievertan00 <ievertan00@gmail.com>
2026-05-12 18:06:34 +0800

    feat: core generation commands

--- bin/wiki.js
+++ bin/wiki.js
@@ -2,12 +2,15 @@
 import { Command } from 'commander';
 import chalk from 'chalk';
 import { loadConfig } from '../src/config.js';
-import { initVault } from '../src/vault.js';
+import { initVault, getVaultFiles, appendLog } from '../src/vault.js';
+import { generateNote } from '../src/llm.js';
+import { saveNote } from '../src/note.js';
 
 const program = new Command();
+let config;
 
 try {
-  const config = loadConfig();
+  config = loadConfig();
   initVault(config.wikiPath);
 } catch (e) {
   console.error(chalk.red(e.message));
@@ -18,5 +21,21 @@ program
   .name('wiki')
   .description('Personal Wiki CLI')
   .version('1.0.0');
+
+const COMMANDS = ['how', 'what', 'why', 'fact'];
+
+COMMANDS.forEach(type => {
+  program
+    .command(type)
+    .argument('<topic>')
+    .action(async (topic) => {
+      console.log(chalk.blue(`Generating ${type} note for: ${topic}...`));
+      const existingFiles = getVaultFiles(config.wikiPath);
+      const content = await generateNote(config, { type, topic, existingFiles });
+      const path = saveNote(config.wikiPath, { type, title: topic, content });
+      appendLog(config.wikiPath, `Created ${type} note: ${topic}`);
+      console.log(chalk.green(`Saved to: ${path}`));
+    });
+});
 
 program.parse();
